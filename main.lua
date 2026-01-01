d77103a5ec4bf0d971b5574f3caa32482f221c7c
siempreavion-dotcom <siempreavion@gmail.com>
2026-01-01 12:05:18 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,4 @@
--- ==========================================
--- AXELBLADIS HUB - DA HOOD ULTIMATE (SKIDO STYLE)
--- ==========================================
-
--- Limpieza de GUIs antiguas para que no se encimen
-if game.CoreGui:FindFirstChild("Orion") then game.CoreGui:FindFirstChild("Orion"):Destroy() end
-
+-- AXELBLADIS HUB DA HOOD EDITION
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 local Window = OrionLib:MakeWindow({
@@ -12,75 +6,63 @@ local Window = OrionLib:MakeWindow({
     HidePremium = false, 
     SaveConfig = true, 
     ConfigFolder = "AxelConfig",
-    IntroText = "AxelBladis Hub Cargando..."
+    IntroText = "Bienvenido AxelBladis"
 })
 
--- PESTAÑA: COMBATE
+-- PESTAÑA DE COMBATE
 local Tab1 = Window:MakeTab({
-	Name = "Combat",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Combat",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 Tab1:AddToggle({
-	Name = "Aimlock / Silent Aim",
-	Default = false,
-	Callback = function(Value)
-		_G.AimEnabled = Value
-        OrionLib:MakeNotification({
-            Name = "Status",
-            Content = "Aimlock: " .. tostring(Value),
-            Duration = 3
-        })
-	end    
+    Name = "Silent Aim / Aimlock",
+    Default = false,
+    Callback = function(Value)
+        _G.AimEnabled = Value
+    end    
 })
 
--- PESTAÑA: MOVIMIENTO (WALKSPEED)
+-- PESTAÑA DE MOVIMIENTO
 local Tab2 = Window:MakeTab({
-	Name = "Movement",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Movement",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 Tab2:AddSlider({
-	Name = "WalkSpeed",
-	Min = 16,
-	Max = 200,
-	Default = 16,
-	Color = Color3.fromRGB(255,0,0),
-	Increment = 1,
-	ValueName = "Speed",
-	Callback = function(Value)
-		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
-	end    
+    Name = "Speed (Velocidad)",
+    Min = 16,
+    Max = 200,
+    Default = 16,
+    Color = Color3.fromRGB(255,0,0),
+    Increment = 1,
+    ValueName = "WalkSpeed",
+    Callback = function(Value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+    end    
 })
 
-Tab2:AddButton({
-	Name = "Fly (Press L to Toggle Hub)",
-	Callback = function()
-        print("Sistema de vuelo listo")
-  	end    
-})
-
--- PESTAÑA: TELEPORTES (DA HOOD)
+-- PESTAÑA DE TELEPORTS
 local Tab3 = Window:MakeTab({
-	Name = "Teleports",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
+    Name = "Teleports",
+    Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 Tab3:AddButton({
-	Name = "Bank",
-	Callback = function()
+    Name = "Teleport to Bank",
+    Callback = function()
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-402, 21, -356)
-  	end    
+    end    
 })
 
 Tab3:AddButton({
-	Name = "Gun Shop",
-	Callback = function()
+    Name = "Teleport to Gun Shop",
+    Callback = function()
         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-581, 7, -736)
-  	end    
+    end    
 })
 
 OrionLib:Init()
